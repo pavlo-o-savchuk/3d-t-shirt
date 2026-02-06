71ebc7a1247b35613086f2220ebb3f2d080badcb
Pavlo Savchuk <pavlo.o.savchuk@gmail.com>
2026-02-06 12:20:03 +0000

    build: new version

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,7 @@
 const config = {
   development: {
-    backendUrl: "http://localhost:8080/api/v1/dalle",
+    backendUrl: "https://3d-t-shirt.up.railway.app/api/v1/dalle",
+    // backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
     backendUrl: "https://3d-t-shirt.up.railway.app/api/v1/dalle",
