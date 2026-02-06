19738c9a6a349151e7a1b407dc48112c10fb9d8d
Pavlo Savchuk <pavlo.o.savchuk@gmail.com>
2026-02-06 12:09:30 +0000

    feat: update the packages

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limig: "5mb" }))
+app.use(express.json({ limit: "5mb" }))
 
 app.use("/api/v1/dalle", dalleRoutes);
 
@@ -16,6 +16,4 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: "Hello from DALL.E" })
 })
 
-//app.listen(8080, () => console.log('Server has started on port 8080'))
-//app.listen(8080, () => console.log('Server has started on port 8080'))
 app.listen(3000, () => console.log('Server has started on port 3000'))
--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,16 +1,14 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({
+const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
-
-const openai = new OpenAIApi(config);
 
 router.route('/').get((req, res) => {
     res.status(200).json({ message: "Hello from DALL.E ROUTES" })
@@ -20,14 +18,15 @@ router.route('/').post(async (req, res) => {
     try {
         const { prompt } = req.body;
 
-        const response = await openai.createImage({
+        const response = await openai.images.generate({
+            model: 'dall-e-2',
             prompt,
             n: 1,
             size: '1024x1024',
             response_format: 'b64_json'
         })
 
-        const image = response.data.data[0].b64_json;
+        const image = response.data[0].b64_json;
         res.status(200).json({ photo: image })
 
     } catch (error) {
